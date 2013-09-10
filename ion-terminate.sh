4bbd61d949408a29651c42c70adc666e589739cb
Adam Brust <abrust@ucsd.edu>
2013-09-10 16:01:40 -0700

    Update ion-terminate script

--- ion-terminate.sh
+++ ion-terminate.sh
@@ -2,7 +2,7 @@
 ##################################################################################
 # ion-terminate.sh
 #
-# This script terinates the ION system
+# This script terminates the ION system
 #
 ##################################################################################
 
@@ -17,16 +17,18 @@ ION_NAME=$1
 # Source ENV
 source ./build-env.sh $ION_NAME
 
-# Create log dir
-#echo "BUILDLOG $BUILD_LOG"
-#mkdir -p $BUILD_LOG
-
 # Setup virtualenv
 . $ION_NAME/bin/activate
 
 # Check if system has already been launched
 if [ -e $HOME/.cloudinitd/cloudinitd-$RUN.db ]; then 
-    # remove node from nagios
+    #### Disable ion-ux
+    echo "Disable ion-ux..."
+    ssh -t $UX_HOST "sudo /www/ux-maintenance.sh offline"
+
+    # remove nodes from nagios
+    echo "Removing nagios nodes..."
+    ssh $NAGIOS_HOST "sudo /root/bin/remove-ion-nodes.sh ion-beta"
 
     # terminate ION system
     echo "Terminating ${ION_NAME}..."
