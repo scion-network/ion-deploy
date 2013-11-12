828d8437d0939225e1c44451e0184036ac513df9
Adam Brust <abrust@ucsd.edu>
2013-11-12 15:46:22 -0800

    mis configured assetmappings for preload

--- build-env.sh
+++ build-env.sh
@@ -46,7 +46,7 @@ case $ION_NAME in
 	export COI_TAR="http://plrepo.oceanobservatories.org/releases/coi-services-ooici-REL2.0-$COI_VERS.tar.gz"
 	export PRELOAD_KEY="0ArzZOLNhEGVqdGUtdHBJLWRTTFVObW1pU25ZTGhQNGc"
         export ASSET_MAP_KEY="0ArzZOLNhEGVqdEZqWVdZSVdFZFkzSS02RDNPMVo1Ync"
-	export PRELOAD="bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader cfg=res/preload/r2_ioc/config/ooi_beta.yml path='http://docs.google.com/spreadsheet/pub?key=${PRELOAD_KEY}&output=xls' assetmappings='http://docs.google.com/spreadsheet/pub?key=${ASSET_MAP_KEY}&output=xls'"
+	export PRELOAD="bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader assets=res/preload/r2_ioc/ooi_assets cfg=res/preload/r2_ioc/config/ooi_beta.yml path='http://docs.google.com/spreadsheet/pub?key=${PRELOAD_KEY}&output=xls' assetmappings='http://docs.google.com/spreadsheet/pub?key=${ASSET_MAP_KEY}&output=xls'"
   	;;
   # ion-beta
   beta)
@@ -67,7 +67,7 @@ case $ION_NAME in
 	export COI_TAR="http://sddevrepo.oceanobservatories.org/releases/coi-services-ooici-REL2.0-$COI_VERS.tar.gz"
 	export PRELOAD_KEY="0ArzZOLNhEGVqdGUtdHBJLWRTTFVObW1pU25ZTGhQNGc"
         export ASSET_MAP_KEY="0ArzZOLNhEGVqdEZqWVdZSVdFZFkzSS02RDNPMVo1Ync"
-	export PRELOAD="bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader cfg=res/preload/r2_ioc/config/ooi_beta.yml path='http://docs.google.com/spreadsheet/pub?key=${PRELOAD_KEY}&output=xls' assetmappings='http://docs.google.com/spreadsheet/pub?key=${ASSET_MAP_KEY}&output=xls'"
+	export PRELOAD="bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader assets=res/preload/r2_ioc/ooi_assets cfg=res/preload/r2_ioc/config/ooi_beta.yml path='http://docs.google.com/spreadsheet/pub?key=${PRELOAD_KEY}&output=xls' assetmappings='http://docs.google.com/spreadsheet/pub?key=${ASSET_MAP_KEY}&output=xls'"
   	;;
   *)
   	echo "Not a valid ion system name. Aborting..."
