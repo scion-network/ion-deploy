a1eb1396099732f67ae366180f003b9f086df242
Adam Brust <abrust@ucsd.edu>
2013-11-12 10:10:14 -0800

    Add assetmappings to preload

--- build-env.sh
+++ build-env.sh
@@ -45,7 +45,8 @@ case $ION_NAME in
 	export EPUAGENT_VERS="2.0.2"
 	export COI_TAR="http://plrepo.oceanobservatories.org/releases/coi-services-ooici-REL2.0-$COI_VERS.tar.gz"
 	export PRELOAD_KEY="0Aq_8oD79eIi4dFlNeGFkRlF5NVVLRjdrUzE1SlBPeHc"
-	export PRELOAD="bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader cfg=res/preload/r2_ioc/config/ooi_beta.yml path='http://docs.google.com/spreadsheet/pub?key=${PRELOAD_KEY}&output=xls'"
+        export ASSET_MAP_KEY="0ArzZOLNhEGVqdEZqWVdZSVdFZFkzSS02RDNPMVo1Ync"
+	export PRELOAD="bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader cfg=res/preload/r2_ioc/config/ooi_beta.yml path='http://docs.google.com/spreadsheet/pub?key=${PRELOAD_KEY}&output=xls' assetmappings='http://docs.google.com/spreadsheet/pub?key=${ASSET_MAP_KEY}&output=xls'"
   	;;
   # ion-beta
   beta)
@@ -65,7 +66,8 @@ case $ION_NAME in
 	export EPUAGENT_VERS="2.0.2"
 	export COI_TAR="http://sddevrepo.oceanobservatories.org/releases/coi-services-ooici-REL2.0-$COI_VERS.tar.gz"
 	export PRELOAD_KEY="0Aq_8oD79eIi4dFlNeGFkRlF5NVVLRjdrUzE1SlBPeHc"
-	export PRELOAD="bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader cfg=res/preload/r2_ioc/config/ooi_beta.yml path='https://docs.google.com/spreadsheet/pub?key=${PRELOAD_KEY}&output=xls'"
+        export ASSET_MAP_KEY="0ArzZOLNhEGVqdEZqWVdZSVdFZFkzSS02RDNPMVo1Ync"
+	export PRELOAD="bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader cfg=res/preload/r2_ioc/config/ooi_beta.yml path='http://docs.google.com/spreadsheet/pub?key=${PRELOAD_KEY}&output=xls' assetmappings='http://docs.google.com/spreadsheet/pub?key=${ASSET_MAP_KEY}&output=xls'"
   	;;
   *)
   	echo "Not a valid ion system name. Aborting..."
