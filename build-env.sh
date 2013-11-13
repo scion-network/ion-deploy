a47bbf2f4c7184578a3ed98c0644dc19ce7da35d
Adam Brust <abrust@ucsd.edu>
2013-11-12 17:45:33 -0800

    new preload key for beta launch

--- build-env.sh
+++ build-env.sh
@@ -44,7 +44,7 @@ case $ION_NAME in
 	export EPU_VERS="2.0.1"
 	export EPUAGENT_VERS="2.0.2"
 	export COI_TAR="http://plrepo.oceanobservatories.org/releases/coi-services-ooici-REL2.0-$COI_VERS.tar.gz"
-	export PRELOAD_KEY="0ArzZOLNhEGVqdGUtdHBJLWRTTFVObW1pU25ZTGhQNGc"
+	export PRELOAD_KEY="0AttCeOvLP6XMdHotSE9YS0NreDZpNFBvT2NBVFhiVGc"
         export ASSET_MAP_KEY="0ArzZOLNhEGVqdEZqWVdZSVdFZFkzSS02RDNPMVo1Ync"
 	export PRELOAD="bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader assets=res/preload/r2_ioc/ooi_assets cfg=res/preload/r2_ioc/config/ooi_beta.yml path='http://docs.google.com/spreadsheet/pub?key=${PRELOAD_KEY}&output=xls' assetmappings='http://docs.google.com/spreadsheet/pub?key=${ASSET_MAP_KEY}&output=xls'"
   	;;
@@ -65,7 +65,7 @@ case $ION_NAME in
 	export EPU_VERS="2.0.1"
 	export EPUAGENT_VERS="2.0.2"
 	export COI_TAR="http://sddevrepo.oceanobservatories.org/releases/coi-services-ooici-REL2.0-$COI_VERS.tar.gz"
-	export PRELOAD_KEY="0ArzZOLNhEGVqdGUtdHBJLWRTTFVObW1pU25ZTGhQNGc"
+	export PRELOAD_KEY="0AttCeOvLP6XMdHotSE9YS0NreDZpNFBvT2NBVFhiVGc"
         export ASSET_MAP_KEY="0ArzZOLNhEGVqdEZqWVdZSVdFZFkzSS02RDNPMVo1Ync"
 	export PRELOAD="bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader assets=res/preload/r2_ioc/ooi_assets cfg=res/preload/r2_ioc/config/ooi_beta.yml path='http://docs.google.com/spreadsheet/pub?key=${PRELOAD_KEY}&output=xls' assetmappings='http://docs.google.com/spreadsheet/pub?key=${ASSET_MAP_KEY}&output=xls'"
   	;;
