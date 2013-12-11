938506bda90c4d1bcd8b03857aab02d818a62ee1
Adam Brust <abrust@ucsd.edu>
2013-12-10 17:05:56 -0800

    bump coi,preload,assetmap for next deploy

--- build-env.sh
+++ build-env.sh
@@ -39,13 +39,13 @@ case $ION_NAME in
 	export NAGIOS_HOST=nagios-pl.oceanobservatories.org
         export SG_HOST=sg.s.oceanobservatories.org
 	export UX_HOST=ooin.oceanobservatories.org
-	export COI_VERS="2.0.82"
+	export COI_VERS="2.0.88"
 	export DTDATA_VERS="2.0.1"
 	export EPU_VERS="2.0.1"
 	export EPUAGENT_VERS="2.0.2"
 	export COI_TAR="http://plrepo.oceanobservatories.org/releases/coi-services-ooici-REL2.0-$COI_VERS.tar.gz"
-	export PRELOAD_KEY="0ArzZOLNhEGVqdDUyMENnZEh3ajRXUFU1Z0xWM1pFY0E"
-        export ASSET_MAP_KEY="0ArzZOLNhEGVqdFp2MnNNMTFSQS0yMUlVYmtBSTFVdXc"
+	export PRELOAD_KEY="0ArzZOLNhEGVqdE5zaGhGS3Q2ZFhoRk1rSlpsaXBULXc"
+        export ASSET_MAP_KEY="0ArzZOLNhEGVqdHA2MHNSX1dlT2ZTaHVrNVJzOG4xZnc"
 	export PRELOAD="bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader assets=res/preload/r2_ioc/ooi_assets cfg=res/preload/r2_ioc/config/ooi_beta.yml path='http://docs.google.com/spreadsheet/pub?key=${PRELOAD_KEY}&output=xls' assetmappings='http://docs.google.com/spreadsheet/pub?key=${ASSET_MAP_KEY}&output=xls'"
   	;;
   # ion-beta
@@ -60,13 +60,13 @@ case $ION_NAME in
 	export NAGIOS_HOST=nagios-sd.oceanobservatories.org
         export SG_HOST=sg.b.oceanobservatories.org
 	export UX_HOST=ion-beta.oceanobservatories.org
-	export COI_VERS="2.0.82"
+	export COI_VERS="2.0.88"
 	export DTDATA_VERS="2.0.1"
 	export EPU_VERS="2.0.1"
 	export EPUAGENT_VERS="2.0.2"
 	export COI_TAR="http://sddevrepo.oceanobservatories.org/releases/coi-services-ooici-REL2.0-$COI_VERS.tar.gz"
-	export PRELOAD_KEY="0ArzZOLNhEGVqdDUyMENnZEh3ajRXUFU1Z0xWM1pFY0E"
-        export ASSET_MAP_KEY="0ArzZOLNhEGVqdFp2MnNNMTFSQS0yMUlVYmtBSTFVdXc"
+	export PRELOAD_KEY="0ArzZOLNhEGVqdE5zaGhGS3Q2ZFhoRk1rSlpsaXBULXc"
+        export ASSET_MAP_KEY="0ArzZOLNhEGVqdHA2MHNSX1dlT2ZTaHVrNVJzOG4xZnc"
 	export PRELOAD="bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader assets=res/preload/r2_ioc/ooi_assets cfg=res/preload/r2_ioc/config/ooi_beta.yml path='http://docs.google.com/spreadsheet/pub?key=${PRELOAD_KEY}&output=xls' assetmappings='http://docs.google.com/spreadsheet/pub?key=${ASSET_MAP_KEY}&output=xls'"
   	;;
   *)
