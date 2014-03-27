e892d153d553bd2987af682ba869c286556da5b2
Jamie Chen <jachen@ucsd.edu>
2014-03-27 12:01:25 -0700

    Update local pre scripts to approximate

--- deployment/2.2_CP02_01/run_local_pre_alpha2.sh
+++ deployment/2.2_CP02_01/run_local_pre_alpha2.sh
@@ -1,8 +1,16 @@
 #!bin/bash
 set -e
+set -x
 this_dir="$(dirname "$0")"
-echo 'Preload ion alpha on a old commit'
+
+echo 'Run below steps on an old commit'
+echo 'Run manually: bin/pycc --rel res/deploy/r2deploy.yml -fc'
+
+echo 'Preload ion alpha'
 sh $this_dir/preload_local_alpha.sh
 
-echo 'Run alpha integration 1 scripts'
+echo 'Run below steps with a new commit'
+echo 'Run bin/pycc --rel res/deploy/r2deploy.yml bootmode=restart'
+
+echo 'Run alpha integration 1 scripts...This is just an approximation'
 sh $this_dir/run_alpha1.sh
--- deployment/2.2_CP02_01/run_local_pre_beta1.sh
+++ deployment/2.2_CP02_01/run_local_pre_beta1.sh
@@ -1,6 +1,11 @@
 #!bin/bash
 set -e
+set -x
 this_dir="$(dirname "$0")"
 
-echo 'Preload ion beta in an old commit'
+echo 'Run below steps on an old commit'
+echo 'Run bin/pycc --rel res/deploy/r2deploy.yml -fc'
+
 sh $this_dir/preload_local_beta.sh
+
+echo 'Now change to a new commit before next script'
