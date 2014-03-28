c03164a57aeddc4299a22f42a098fe8765059185
Jamie Chen <jachen@ucsd.edu>
2014-03-27 17:02:53 -0700

    Simulate pre beta locally in one script

--- deployment/2.2_CP02_01/run_local_pre_beta1.sh
+++ deployment/2.2_CP02_01/run_local_pre_beta1.sh
@@ -1,13 +1,45 @@
 #!bin/bash
 set -e
 set -x
+trap 'my_exit; exit' SIGUSR1
+count=0
 this_dir="$(dirname "$0")"
 
 echo 'Run below steps on an old commit'
-echo 'Run bin/pycc --rel res/deploy/r2deploy.yml -fc'
+git fetch origin
+git checkout v2.1.2
+git submodule update
+ant clean
+bin/generate_interfaces
 
+echo 'Start r2 deploy in no shell mode with signaling to parent'
+bin/pycc -sp -n --rel res/deploy/r2deploy.yml -fc &
+r2deploy_pid=$!
+
+my_exit()
+{
+echo $r2deploy_pid caught
 echo 'Preload local beta'
-bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader cfg=res/preload/r2_ioc/config/ooi_beta.yml path="https://docs.google.com/spreadsheet/pub?key=0Aq_8oD79eIi4dHpTOGV2bGZVNkJXd0J1ci1SX25zNXc&output=xls" assetmappings="https://docs.google.com/spreadsheet/pub?key=0ArzZOLNhEGVqdHA2MHNSX1dlT2ZTaHVrNVJzOG4xZnc&output=xls"
+bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader cfg=res/preload/r2_ioc/config/ooi_beta.yml path="https://docs.google.com/spreadsheet/pub?key=0ArzZOLNhEGVqdE5zaGhGS3Q2ZFhoRk1rSlpsaXBULXc&output=xls" assetmappings="https://docs.google.com/spreadsheet/pub?key=0ArzZOLNhEGVqdHA2MHNSX1dlT2ZTaHVrNVJzOG4xZnc&output=xls"
+
+
+sleep 5
+echo 'Kill the r2 deploy'
+kill $r2deploy_pid
 
 echo 'Now manually change to a new commit before running next step'
-echo 'Run bin/pycc --rel res/deploy/r2deploy.yml bootmode=restart --mx'
+git checkout REL2.0
+git checkout origin/REL2.0
+git submodule update
+ant clean
+bin/generate_interfaces
+
+bin/pycc --rel  res/deploy/r2deploy.yml bootmode=restart --mx
+}
+
+while :
+do
+    sleep 5
+    count=$(expr $count + 1)
+    echo $count
+done
