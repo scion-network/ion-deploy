4cfecb304b09de09bd0fd1904c46bf724e50d458
Jamie Chen <jachen@ucsd.edu>
2014-04-01 08:25:55 -0700

    Set path to master

--- deployment/2.2_CP02_01/run_alpha2.sh
+++ deployment/2.2_CP02_01/run_alpha2.sh
@@ -13,7 +13,7 @@ echo 'Remove resources and coverage and agent state for 6 sites'
 bin/pycc -x ion.agents.agentctrl.AgentControl force=True recurse=True preload_id="CP02PMUI-WF001_PD,CP02PMUO-WF001_PD,CP02PMCI-WF001_PD,CP02PMCO-WF001_PD,CE09OSPM-WF001_PD,CP04OSPM-WF001_PD" op=delete_all_site
 
 echo 'Run incremental preload'
-bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader cfg=$thisdir/ooi_inc_preload.yml
+bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader cfg=$thisdir/ooi_inc_preload.yml path=master
 
 echo 'run calibration for 4 sites'
 bin/pycc -x ion.agents.agentctrl.AgentControl force=True recurse=True preload_id="CP02PMUI-WP001_PD,CP02PMUO-WP001_PD,CP02PMCI-WP001_PD,CP02PMCO-WP001_PD" op=set_calibration cfg=$thisdir/calibration2.csv
