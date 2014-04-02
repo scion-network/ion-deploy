d736e2e217bb504482a52757db6fc576fb56a575
Jamie Chen <jachen@ucsd.edu>
2014-04-02 12:08:01 -0700

    Download googlesheets first for beta to save some time

--- deployment/2.2_CP02_01/run_beta1.sh
+++ deployment/2.2_CP02_01/run_beta1.sh
@@ -9,17 +9,30 @@ bin/pycc -x ion.agents.agentctrl.AgentControl force=True recurse=True preload_id
 echo 'Deactivate persistence for 2 sites'
 bin/pycc -x ion.agents.agentctrl.AgentControl force=True recurse=True preload_id="CE09OSPM-WF001_PD,CP04OSPM-WF001_PD" op=suspend_persistence
 
+# MASTER paths
+
+rm -f /tmp/preload.xlsx
+rm -f /tmp/assetmappings.xlsx
+preload_path="https://docs.google.com/spreadsheet/pub?key=0AttCeOvLP6XMdG82NHZfSEJJOGdQTkgzb05aRjkzMEE&output=xls"
+curl -o /tmp/preload.xlsx $preload_path
+assetmappings_path="https://docs.google.com/spreadsheet/pub?key=0AttCeOvLP6XMdFVUeDdoUTU0b0NFQ1dCVDhuUjY0THc&output=xls"
+curl -o /tmp/assetmappings.xlsx $assetmappings_path
+
+# I had to add this step since inc preload 1 has deployment error
+echo 'Remove resources and coverage and agent state for 6 WP and 6 WF sites'
+bin/pycc -x ion.agents.agentctrl.AgentControl force=True recurse=True preload_id="CP02PMUI-WP001_PD,CP02PMUO-WP001_PD,CP02PMCI-WP001_PD,CP02PMCO-WP001_PD,CE09OSPM-WP001_PD,CP04OSPM-WP001_PD,CP02PMUI-WF001_PD,CP02PMUO-WF001_PD,CP02PMCI-WF001_PD,CP02PMCO-WF001_PD,CE09OSPM-WF001_PD,CP04OSPM-WF001_PD" op=delete_all_site
+
 echo 'Run inc preload 1'
-bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader cfg=$thisdir/ooi_inc_preload.yml path=master
+bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader cfg=$thisdir/ooi_inc_preload.yml path=/tmp/preload.xlsx assetmappings=/tmp/assetmappings.xlsx
 
 echo 'Remove resources and coverage and agent state for 6 WP and 6 WF sites'
 bin/pycc -x ion.agents.agentctrl.AgentControl force=True recurse=True preload_id="CP02PMUI-WP001_PD,CP02PMUO-WP001_PD,CP02PMCI-WP001_PD,CP02PMCO-WP001_PD,CE09OSPM-WP001_PD,CP04OSPM-WP001_PD,CP02PMUI-WF001_PD,CP02PMUO-WF001_PD,CP02PMCI-WF001_PD,CP02PMCO-WF001_PD,CE09OSPM-WF001_PD,CP04OSPM-WF001_PD" op=delete_all_site
 
 echo 'Run wfp preload'
-bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader cfg=$thisdir/wfp_preload.yml path=master
+bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader cfg=$thisdir/wfp_preload.yml path=/tmp/preload.xlsx assetmappings=/tmp/assetmappings.xlsx
 
 echo 'Run inc preload 2'
-bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader cfg=$thisdir/ooi_inc_preload.yml path=master
+bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader cfg=$thisdir/ooi_inc_preload.yml path=/tmp/preload.xlsx assetmappings=/tmp/assetmappings.xlsx
 
 
 echo 'run calibration for 4 instruments and 2 pds'
