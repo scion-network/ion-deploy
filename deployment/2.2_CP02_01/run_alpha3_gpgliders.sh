e0cd98cfbbe6c95fad6522a8efae60b8133508e5
Michael Meisinger <mmeisinger@ucsd.edu>
2014-04-04 14:45:58 -0700

    Added recurse=True

--- deployment/2.2_CP02_01/run_alpha3_gpgliders.sh
+++ deployment/2.2_CP02_01/run_alpha3_gpgliders.sh
@@ -28,8 +28,8 @@ echo 'Run OOI preload incrementally - this will fill in the gaps after the delet
 bin/pycc -x ion.processes.bootstrap.ion_loader.IONLoader cfg=$thisdir/ooi_inc_preload.yml path=/tmp/preload.xlsx assetmappings=/tmp/assetmappings.xlsx
 
 echo 'Configure agent instances for 3 GP05 glider platform devices'
-bin/pycc -x ion.agents.agentctrl.AgentControl preload_id="GP05MOAS-GL001_PD,GP05MOAS-GL002_PD,GP05MOAS-GL003_PD" op=config_instance cfg=$thisdir/eai_configs.csv
+bin/pycc -x ion.agents.agentctrl.AgentControl recurse=True preload_id="GP05MOAS-GL001_PD,GP05MOAS-GL002_PD,GP05MOAS-GL003_PD" op=config_instance cfg=$thisdir/eai_configs.csv
 
 echo 'Start agents for 3 GP05 glider platform devices'
-bin/pycc -x ion.agents.agentctrl.AgentControl preload_id="GP05MOAS-GL001_PD,GP05MOAS-GL002_PD,GP05MOAS-GL003_PD" op=start
+bin/pycc -x ion.agents.agentctrl.AgentControl recurse=True preload_id="GP05MOAS-GL001_PD,GP05MOAS-GL002_PD,GP05MOAS-GL003_PD" op=start
 
