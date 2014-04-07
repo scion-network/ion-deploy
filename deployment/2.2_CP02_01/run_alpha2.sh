5de84cfe04c45a95df76cc215eadec30fec71c00
Jamie Chen <jachen@ucsd.edu>
2014-04-07 11:41:26 -0700

    Add copy of preload to alpha and beta

--- deployment/2.2_CP02_01/run_alpha2.sh
+++ deployment/2.2_CP02_01/run_alpha2.sh
@@ -7,6 +7,10 @@ thisdir="$(dirname "$0")"
 preload_path="https://docs.google.com/spreadsheet/pub?key=0AttCeOvLP6XMdG82NHZfSEJJOGdQTkgzb05aRjkzMEE&output=xls"
 assetmappings_path="https://docs.google.com/spreadsheet/pub?key=0AttCeOvLP6XMdFVUeDdoUTU0b0NFQ1dCVDhuUjY0THc&output=xls"
 
+# Copy of master
+preload_path="https://docs.google.com/spreadsheet/pub?key=0ArzZOLNhEGVqdGptdl8tVk1QdHl0d01oYTY0aGJTMVE&output=xls"
+assetmappings_path="https://docs.google.com/spreadsheet/pub?key=0ArzZOLNhEGVqdHpVSmtIUE5UeHNUTVBObmd4WlBmaHc&output=xls"
+
 rm -f /tmp/preload.xlsx
 rm -f /tmp/assetmappings.xlsx
 curl -o /tmp/preload.xlsx $preload_path
